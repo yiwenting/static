c8a185eb596cd1f37d8db7bb7bae17aa2e36d452
Wenting Yi <iamyiwenting@gmail.com>
2016-10-09 16:18:14 +0000

    config

--- business/mailer/index.js
+++ business/mailer/index.js
@@ -1,8 +1,11 @@
 var transporter = require('./transporter');
+var config = require('../../config/config.json');
 
 function send(subject, htmlContent, done) {
     // setup e-mail data with unicode symbols
     var mailOptions = {
+        from: '"' + config.owner.name + '" <' + config.owner.email + '>',
+        to: config.owner.email,
         subject: subject, // Subject line
         html: htmlContent // html body
     };
